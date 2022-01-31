363f4e0fe6c1ed004a436ea6eb237d48a5f16c70
SRaven1994 <92119448+SRaven1994@users.noreply.github.com>
2022-01-31 12:42:06 +0000

    Map and Player Loaded
    Tilemap and the player have been loaded into the project. Player has basic touch control enable for movement.

--- js/SceneA.js
+++ js/SceneA.js
@@ -1,2 +1,9 @@
 class SceneA extends BaseScene{
+    /** @type {string} */
+    static sceneID = "sceneA"
+    constructor(){
+        super(SceneA.sceneID)
+        this.tileDataKey = "slopes"
+        this.tileDataSource = "assets/tilemaps/slopes.json"
+    }
 }
